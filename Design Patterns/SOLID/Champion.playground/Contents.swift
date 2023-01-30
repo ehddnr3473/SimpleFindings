7f8544c48793517baf3a6def476a0b9193ad7ff8
Donguk Kim <ehddnr73@naver.com>
2023-01-30 11:02:34 +0900

    Liscov substitution principle 사례 코드 수정

--- Design Patterns/SOLID/Champion.playground/Contents.swift
+++ Design Patterns/SOLID/Champion.playground/Contents.swift
@@ -2,34 +2,40 @@ import UIKit
 
 // Base class / Superclass
 class Champion {
-    func speak() {
-        print("챔피언")
+    private let name: String
+    private let dialogue: String
+    private var hitPoint: Int
+    
+    init(name: String, dialogue: String, hitPoint: Int) {
+        self.name = name
+        self.dialogue = dialogue
+        self.hitPoint = hitPoint
+    }
+    
+    func speak() -> (String, String) {
+        (name, dialogue)
     }
 }
 
-// Subclass
-class Jayce: Champion {
-    override func speak() {
-        print("안녕하세요, 제이스입니다.")
-    }
-}
-
-// Subclass
-class Minion: Champion {
-    override func speak() {
+// Subclasses
+class Jayce: Champion { }
+class Lucian: Champion { }
+class MeleeMinion: Champion {
+    override func speak() -> (String, String) {
         fatalError("저는 말을 못해요.")
     }
 }
 
 func speak(champion: Champion) {
-    champion.speak()
+    let speak = champion.speak()
+    print("나는 \(speak.0). \(speak.1)")
 }
 
-let champion = Champion()
-speak(champion: champion)
-
-let jayce = Jayce()
+let jayce = Jayce(name: "Jayce", dialogue: "안녕하세요.", hitPoint: 590)
 speak(champion: jayce)
 
-let minion = Minion()
-speak(champion: minion)
+let lucian = Lucian(name: "Lucian", dialogue: "싸우자.", hitPoint: 641)
+speak(champion: lucian)
+
+let meleeMinion = MeleeMinion(name: "Melee minion", dialogue: "", hitPoint: 477)
+speak(champion: meleeMinion)
