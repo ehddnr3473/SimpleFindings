194e3720c66a5d75aedcfe0ba15366bef57048b9
Donguk Kim <ehddnr73@naver.com>
2023-03-03 16:52:28 +0900

    Adapter

---
+++ Design Patterns/Structural/Adapter.playground/Contents.swift
@@ -0,0 +1,36 @@
+/*
+ Adapter
+ 클라이언트에 필요한 인터페이스를 지원하는 클래스로, "adaptee"를 래핑하여 호환되지 않는 두 타입 간의 링크를 제공하는 데 사용됨.
+ */
+
+protocol NewFrame {
+    var width: Double { get }
+    var height: Double { get }
+}
+
+struct OldFrameTarget {
+    let width: Float
+    let height: Float
+}
+
+struct NewFrameTarget: NewFrame {
+    private let target: OldFrameTarget
+    
+    var width: Double {
+        Double(target.width) + 10.0
+    }
+    
+    var height: Double {
+        Double(target.height) + 10.0
+    }
+    
+    init(_ target: OldFrameTarget) {
+        self.target = target
+    }
+}
+
+let target = OldFrameTarget(width: 50.0, height: 100.0)
+let newFormat = NewFrameTarget(target)
+
+newFormat.width
+newFormat.height
